7e3ae3094b4fe5fe9c00b9185cd2167df8c3b6c1
Ajay Singh <a.pratap027@gmail.com>
2020-05-21 16:43:53 -0400

    adding before test concept

--- test/heroku.test.js
+++ test/heroku.test.js
@@ -3,16 +3,18 @@ const herokupage = require('../pages/heroku.page');
 
 describe('Heroku app Home page dropdown feature', function(){
 
-
-    it('Validate dropdown by selecting value', function(){
+    beforeEach(function() {
         browser.url('http://the-internet.herokuapp.com/');
         browser.maximizeWindow();
+    });
+
+    it('Validate dropdown by selecting value', function(){
         herokupage.clickDropdownLink();
         herokupage.selectValue('1');
         browser.pause(2000);
         console.log(herokupage.categoryDropdown.getValue());
-    })
+    });
 
 
     // it('')
-})
+});
